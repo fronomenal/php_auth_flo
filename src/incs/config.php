01203b415a105b5e61ad4b0ecb17b6e614d74a42
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-12-26 00:02:45 +0000

    initial project setup

---
+++ src/incs/config.php
@@ -0,0 +1,17 @@
+<?php
+
+	// Global Variables
+	define('MAX_LOGIN_ATTEMPTS_PER_HOUR', 5);
+	define('MAX_EMAIL_VERIFICATION_REQUESTS_PER_DAY', 3);
+	define('MAX_PASSWORD_RESET_REQUESTS_PER_DAY', 3);
+	define('PASSWORD_RESET_REQUEST_EXPIRY_TIME', 60*60);
+	define('CSRF_TOKEN_SECRET', 'somethingsomethingsomesecret');
+	define('VALIDATE_EMAIL_ENDPOINT', 'http://localhost:8080/pages/validateEmail');
+	define('RESET_PASSWORD_ENDPOINT', 'http://localhost:8080/pages//resetpassword');
+
+	// Page Startup Scripts
+	date_default_timezone_set('UTC'); 
+	error_reporting(0);
+	session_set_cookie_params(['samesite' => 'Strict']);
+	session_start();
+	
